a32ed1a76df47451ab4d50ef556d53070dcd80a6
GrupoDukaShop <GrupoDukaShop@users.noreply.github.com>
2026-08-21 22:04:09 -0300

    Allow checkout route analytics to load

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,7 +1,17 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
 
 const checkoutUrl = "https://pay.hotmart.com/T107258212F?checkoutMode=2";
 
 export default function CheckoutRedirect() {
-  redirect(checkoutUrl);
+  useEffect(() => {
+    const redirectTimer = window.setTimeout(() => {
+      window.location.replace(checkoutUrl);
+    }, 500);
+
+    return () => window.clearTimeout(redirectTimer);
+  }, []);
+
+  return null;
 }
